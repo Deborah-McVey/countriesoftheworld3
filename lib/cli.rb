230ba638277854430a70f9337f342dcc4d93140a
Deborah-McVey <139592532+Deborah-McVey@users.noreply.github.com>
2024-01-20 15:25:50 +0000

    removed "end" from cli file line 73 due to error message

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,5 @@
 require_relative "scraper.rb"
+require_relative "./user.rb"
 
 class CLI
   def start
@@ -19,8 +20,53 @@ class CLI
       puts "Area: #{country.area}"
     end
   end
+  #bcrypt auth
+  # authenticate user or create account
+    def authenticate
+      authenticated = false
+
+      until authenticated
+        puts 'Please login or sign up'
+        puts 'Which do you choose? (sign up/login)'
+
+        if get_input == 'login'
+          authenticated = login
+        else
+          create_account
+        end
+      end
+    end
+
+    # check if user is in User class and if password matches
+    def login
+      puts 'Please enter your username:'
+      username = gets.chomp
+      puts 'Please enter your password:'
+      password = gets.chomp
+      result = User.authenticate(username, password)
+
+      if result
+        puts "Welcome back #{username}!"
+      else
+        puts 'Sorry, that username and password combination is not recognized. Please try again.'
+      end
+      result
+    end
+
+    # create a new user and add to User class
+    def create_account
+      puts 'Please enter a username:'
+      username = gets.chomp
+
+      puts 'Please enter a password:'
+      password = gets.chomp
+
+      User.new(username, password)
+      puts 'Account created'
+    end
+
+  end
 
   def get_input
     gets.strip
   end
-end
